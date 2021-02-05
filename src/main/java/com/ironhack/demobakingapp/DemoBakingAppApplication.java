e8f94ca52c832c1895d4730ebf0d0cfa121212d5
Paul Barroso <paulbg1993@gmail.com>
2021-02-05 13:04:57 +0100

    First Commit

---
+++ src/main/java/com/ironhack/demobakingapp/DemoBakingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.demobakingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBakingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/demobakingapp/DemoBakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.demobakingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
